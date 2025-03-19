886bfeb9ee55d8ef79afe92482f696e83362c6f9
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-03-19 22:59:56 +0530

    resolved google login bug

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 import { Google } from 'arctic';
+import { dev } from '$app/environment';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 if (!env.GOOGLE_CLIENT_ID) throw new Error('GOOGLE_CLIENT_ID is not set');
@@ -12,5 +13,7 @@ export const db = drizzle(client);
 export const google = new Google(
 	env.GOOGLE_CLIENT_ID,
 	env.GOOGLE_CLIENT_SECRET,
-	'http://localhost:5173/login/google/callback'
+	dev
+		? 'http://localhost:5173/login/google/callback'
+		: 'https://chitchat-ai.vercel.app/login/google/callback'
 );
